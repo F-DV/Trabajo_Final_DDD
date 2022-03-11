47aa56de6ab2dfe2002bcc7c269e22ff760b2853
Felipe <fquiceno1@misena.edu.co>
2022-03-11 14:29:55 -0500

    Declaramos entidades en agregado Root

--- src/main/java/co/com/sofka/trabajoFinal/cursosVirtuales/marketPlace/Cuenta.java
+++ src/main/java/co/com/sofka/trabajoFinal/cursosVirtuales/marketPlace/Cuenta.java
@@ -2,10 +2,21 @@ package co.com.sofka.trabajoFinal.cursosVirtuales.marketPlace;
 
 import co.com.sofka.domain.generic.AggregateRoot;
 import co.com.sofka.trabajoFinal.cursosVirtuales.marketPlace.values.CuentaId;
+import co.com.sofka.trabajoFinal.cursosVirtuales.marketPlace.values.CursoId;
+import co.com.sofka.trabajoFinal.cursosVirtuales.marketPlace.values.UsuarioId;
+
+import java.util.List;
 
 public class Cuenta extends AggregateRoot<CuentaId> {
+
+    protected List<CursoId> cursos;
+    protected UsuarioId usuarioId;
+    protected Perfil perfil;
+    protected Plan plan;
+    protected Pago pago;
 
     public Cuenta(CuentaId entityId) {
         super(entityId);
     }
+
 }
