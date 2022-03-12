93f1f699a5cd1481622b07f34498f1ab4ecfc886
Felipe <fquiceno1@misena.edu.co>
2022-03-11 22:12:06 -0500

    le agregamos los getters y los comportamientos a la entidad Datos

--- src/main/java/co/com/sofka/trabajoFinal/cursosVirtuales/usuario/Datos.java
+++ src/main/java/co/com/sofka/trabajoFinal/cursosVirtuales/usuario/Datos.java
@@ -1,4 +1,41 @@
 package co.com.sofka.trabajoFinal.cursosVirtuales.usuario;
 
-public class Datos {
+import co.com.sofka.domain.generic.Entity;
+import co.com.sofka.trabajoFinal.cursosVirtuales.usuario.values.DatosId;
+import co.com.sofka.trabajoFinal.cursosVirtuales.usuario.values.Email;
+import co.com.sofka.trabajoFinal.cursosVirtuales.usuario.values.Nombre;
+
+import java.util.Objects;
+
+public class Datos extends Entity<DatosId> {
+
+    private Nombre nombre;
+    private Email email;
+
+    public Datos(DatosId entityId, Nombre nombre, Email email1) {
+        super(entityId);
+        this.nombre = nombre;
+        this.email = email1;
+    }
+
+    /**
+     * Comportamientos
+     */
+    public void actualizarNombre(Nombre nombre){
+        this.nombre = Objects.requireNonNull(nombre);
+    }
+    public void actualizarEmail(Email email){
+        this.email = Objects.requireNonNull(email);
+    }
+
+    /**
+     * Getters
+     */
+    public Nombre Nombre() {
+        return nombre;
+    }
+
+    public Email Email() {
+        return email;
+    }
 }
---
+++ src/main/java/co/com/sofka/trabajoFinal/cursosVirtuales/usuario/values/DatosId.java
@@ -0,0 +1,6 @@
+package co.com.sofka.trabajoFinal.cursosVirtuales.usuario.values;
+
+import co.com.sofka.domain.generic.Identity;
+
+public class DatosId extends Identity {
+}
